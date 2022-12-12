673d08c8f39b5ca22dc5ca7638f3810e6d67a853
pedro fernando fernandes pereira <pedroffpereira2001@gmail.com>
2022-12-12 20:39:10 +0000

    alterações

--- menus/admin/relatorios.cpp
+++ menus/admin/relatorios.cpp
@@ -89,6 +89,172 @@ void relatorioProdutos(string **Products) {
 
 #endif
 
+#ifndef biggestSell_file
+#define biggestSell_file
+
+    void biggestSell(string** Bills) {
+
+        string** Products = allProductsBillsSort(Bills, 4);
+
+        cout << "O produto mais vendido é " << Products[getsize(Products)];
+
+
+        system("pause");
+    }
+
+#endif
+
+#ifndef smallestSell_file
+#define smallestSell_file
+
+       void smallestSell(string** Bills) {
+        
+        
+        string** Products = allProductsBillsSort(Bills, 4);
+
+        cout << "O produto menos vendido é " << Products[0][0];
+
+        
+        system("pause");
+
+    }
+
+#endif
+
+#ifndef biggestbuyer_file
+#define biggestbuyer_file
+
+    void biggestbuyer(string** Bills) {
+
+        //string** clients = allclientsBillsSort(Bills, 4);
+
+        cout << "O Client que comprou mais é " << clients[getsize(clients)][0];
+
+
+        system("pause");
+    }
+
+#endif
+
+#ifndef productProfitSort_file
+#define productProfitSort_file
+
+    void productProfitSort(string** Bills) {
+
+        string** clients = allProductsBillsSort(Bills, 7);
+
+        cout << "O Client que comprou mais é " << clients[getsize(clients)][0];
+
+
+        system("pause");
+    }
+
+#endif
+
+
+#ifndef allProductsBills_file
+#define allProductsBills_file
+
+    string** allProductsBillsSort(string **bill, int pos) {
+        
+        string** Products = new string*[102];
+
+        Products[0] = nullptr;
+
+        for (int i = 0; i < getsize(bill); i++)
+        {
+
+            bool flag = false;
+
+            for (int j = 0; j < getsize(Products); j++)
+            {
+                
+                if(Products[j][0] == bill[i][3]) {
+
+                    Products[j][1] = to_string(stoi(Products[j][1]) + stoi(bill[i][pos]));
+
+                    flag = true;
+
+                }
+
+            }
+
+            if(!flag){
+                delete[] Products[i];
+
+                Products[i] = new string[2];
+
+                Products[i][0] = bill[i][3];
+
+                Products[i][1] = bill[i][pos];
+
+                Products[i + 1] = nullptr;
+            }
+
+        }
+        
+        for(int i=0; i< getsize(Products); i++)
+        {		
+            for(int j=i+1; j < getsize(Products) ; j++)
+            {
+                if(Products[i][1]>Products[j][1])
+                {
+                    string* temp = new string[2];
+
+                    temp  = Products[i];
+                    Products[i]=Products[j];
+                    Products[j]=temp;
+                }
+            }
+        }
+
+
+        return Products;
+    }
+
+
+#endif
+
+#ifndef GeralRelatoriostotal_file
+#define GeralRelatoriostotal_file
+    void GeralRelatoriosTotal(string **bill) {
+        setlocale(LC_ALL, "Portuguese");
+        int action;
+    
+        do {
+
+            system ("CLS");
+
+            cout << "1 - Produto mais vendido" << endl;
+
+            cout << "2 - Produto menos vendido" << endl;
+
+            cout << "3 - Produto com mais lucro" << endl;
+
+            cout << "4 - Cliente que mais comprou" << endl;
+
+            cout << "0 - Sair" << endl;
+
+            action = validateTypeInt();
+
+            switch (action)
+            {
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break;
+            }
+
+
+
+        } while(action != 0);
+    }
+#endif
+
 
 #ifndef GeralRelatorios_file
 #define GeralRelatorios_file
@@ -128,6 +294,9 @@ void GeralRelatorios(string ***GLOBAL)
                 cout << "Por favor insira o nome do produto" << endl;
                 NameProduct = inputSpace();
                 relatorioClientes(GLOBAL[2], NameProduct);
+            break;
+            case 4:
+                GeralRelatoriosTotal(GLOBAL[1]);
             break;
         }
     } while(action != 0);
